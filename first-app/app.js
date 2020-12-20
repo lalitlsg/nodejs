7db8d03333300510866447a084c308892bf74466
Lalit Garghate <lalit.garghate@gmail.com>
2020-12-20 19:16:42 +0530

    path module

--- first-app/app.js
+++ first-app/app.js
@@ -1,2 +1,9 @@
+const path = require("path");
+
 const logger = require("./logger");
-logger.showName("Lalit");
+
+// logger.showName("Laslit");
+
+//path module
+let pathObj = path.parse(__filename);
+console.log(pathObj);
