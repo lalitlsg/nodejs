296b13d9c0cbf3453aafaf7e2871c9d2755294ae
Lalit Garghate <lalit.garghate@gmail.com>
2020-12-20 19:27:10 +0530

    os, fs modules

--- first-app/app.js
+++ first-app/app.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
 const logger = require("./logger");
 
@@ -6,4 +8,13 @@ const logger = require("./logger");
 
 //path module
 let pathObj = path.parse(__filename);
-console.log(pathObj);
+// console.log(pathObj);
+
+//os module
+console.log(`${os.totalmem}, ${os.freemem}`);
+
+//fs module
+fs.readdir("./", (err, files) => {
+  if (err) console.log("Error", err);
+  else console.log("Files", files);
+});
